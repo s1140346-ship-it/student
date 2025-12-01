738519c3cbdd4a73a6381f20ae4a83c6304f9690
s1140346-ship-it <s1140346@dmhs.kh.edu.tw>
2025-12-01 16:00:10 +0800

    hw9.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
-int main(){
-    std::cout << "請輸入年齡";
-    int age;
-    std::cin >> age;
-    if(age >= 18) {
-       std::cout << "你可以觀看pxxxxhub 網站 ";
-       
-    }
-    if (age <18) {
-        std::cout << "你還是好好讀書 ";
-    }
+int main()
+{
+    int sum =0;
+    for(int i=1;i<6;i++) {
+       std::cout << "now i sum:" << i << std::end1;
+       sum = sum + i
+       std::cout << "the number of circle" << sum << std::end1;
+       } 
+       std::cout << "1+..+5 總合為:" << sum;
+    
     return 0;
 }
