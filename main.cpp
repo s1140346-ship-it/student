c0ec39151fefebc8b821c5d92df8a610d45973a8
s1140346-ship-it <s1140346@dmhs.kh.edu.tw>
2025-12-05 11:43:47 +0800

    hw10cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 
 int main()
 {
-    int sum =0;
-    for(int i=1;i<6;i++) {
-       std::cout << "now i sum:" << i << std::end1;
-       sum = sum + i
-       std::cout << "the number of circle" << sum << std::end1;
-       } 
-       std::cout << "1+..+5 總合為:" << sum;
-    
+    for (int i=1;i<=9;++ ) {
+        for (int j=1;<=9;j++) {
+           std::cout << i << "*" << J << "=" i j <<"/t";
+        }
+        std::cout << std::endl;
+        }
     return 0;
 }
